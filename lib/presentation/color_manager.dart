53fbaf7b37eedfb10ed003b02eaf56db7cacdf0b
Molingejr <molingejr@gmail.com>
2022-11-13 12:52:05 +0100

    Add other styles managers

--- lib/presentation/color_manager.dart
+++ lib/presentation/color_manager.dart
@@ -2,10 +2,17 @@ import 'package:flutter/material.dart';
 
 class ColorManager {
   static Color primary = HexColor.fromHex("#ED9728");
-  static Color dark = HexColor.fromHex("#525252");
+  static Color darkGrey = HexColor.fromHex("#525252");
   static Color grey = HexColor.fromHex("#737477");
   static Color lightGrey = HexColor.fromHex("#9E9E9E");
   static Color primaryOpacity70 = HexColor.fromHex("#B3ED9728");
+
+  // new colors
+  static Color darkPrimary = HexColor.fromHex("#D17D11");
+  static Color grey1 = HexColor.fromHex("#707070");
+  static Color grey2 = HexColor.fromHex("#797979");
+  static Color white = HexColor.fromHex("#FFFFFF");
+  static Color error = HexColor.fromHex("#E61F34");
 }
 
 extension HexColor on ColorManager {
