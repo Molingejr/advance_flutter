965eff1dfcb31176db10c008b176338a2ffc6322
Molingejr <molingejr@gmail.com>
2022-11-13 17:27:05 +0100

    Splash screen

--- lib/presentation/splash/splash.dart
+++ lib/presentation/splash/splash.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'package:advance_flutter/presentation/resources/assets_manager.dart';
+import 'package:advance_flutter/presentation/resources/color_manager.dart';
+import 'package:advance_flutter/presentation/resources/routes_manager.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatefulWidget {
@@ -8,8 +12,37 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> {
+  Timer? _timer;
+
+  _startDelay() {
+    _timer = Timer(const Duration(seconds: 2), _goNext);
+  }
+
+  _goNext() {
+    Navigator.pushReplacementNamed(context, Routes.onBoardingRoute);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _startDelay();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: ColorManager.primary,
+      body: const Center(
+        child: Image(
+          image: AssetImage(ImageAssets.splashLogo),
+        ),
+      ),
+    );
   }
 }
