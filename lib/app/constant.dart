c7a6c2addb4d6a46f3e3a523c78683b4bb873e3d
Molingejr <molingejr@gmail.com>
2022-12-14 10:34:55 +0100

    Adding dependencies, api responses, json serialization, and api service client

---
+++ lib/app/constant.dart
@@ -0,0 +1,3 @@
+class Constant {
+  static const String baseUrl = "https://molinge.mocklab.io/";
+}
---
+++ lib/data/network/app_api.dart
@@ -0,0 +1,13 @@
+import 'package:advance_flutter/app/constant.dart';
+import 'package:advance_flutter/data/responses/responses.dart';
+import 'package:retrofit/http.dart';
+import 'package:dio/dio.dart';
+part 'app_api.g.dart';
+
+@RestApi(baseUrl: Constant.baseUrl)
+abstract class AppServiceClient {
+  factory AppServiceClient(Dio dio, {String baseUrl}) = _AppServiceClient;
+
+  @POST("/customers/login")
+  Future<AuthenticationResponse> login();
+}
