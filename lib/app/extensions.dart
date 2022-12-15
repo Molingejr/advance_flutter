9b735285a9a2172ca455cfe961dd0ede2a9a1511
Molingejr <molingejr@gmail.com>
2022-12-15 08:20:37 +0100

    Creating models, extensions, and mappers

---
+++ lib/app/extensions.dart
@@ -0,0 +1,23 @@
+// extension on string
+import 'package:advance_flutter/data/mapper/mapper.dart';
+
+extension NonNullString on String? {
+  String orEmpty() {
+    if (this == null) {
+      return EMPTY;
+    } else {
+      return this!;
+    }
+  }
+}
+
+// extension on Integer
+extension NonNullInteger on int? {
+  int orZero() {
+    if (this == null) {
+      return ZERO;
+    } else {
+      return this!;
+    }
+  }
+}
