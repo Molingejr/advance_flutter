f973e98b943f92f17113b866098238f1408a4e14
Molingejr <molingejr@gmail.com>
2023-03-09 18:12:25 +0100

    minor fixes on other screens localization

--- lib/presentation/main/notifications_page.dart
+++ lib/presentation/main/notifications_page.dart
@@ -1,5 +1,6 @@
 import 'package:advance_flutter/presentation/resources/strings_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 class NotificationsPage extends StatefulWidget {
   const NotificationsPage({Key? key}) : super(key: key);
@@ -12,7 +13,7 @@ class _NotificationsPageState extends State<NotificationsPage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(AppStrings.notifications),
+      child: const Text(AppStrings.notifications).tr(),
     );
   }
 }
--- lib/presentation/main/settings_page.dart
+++ lib/presentation/main/settings_page.dart
@@ -2,12 +2,15 @@ import 'package:advance_flutter/app/app_prefs.dart';
 import 'package:advance_flutter/app/di.dart';
 import 'package:advance_flutter/data/data_source/local_data_source.dart';
 import 'package:advance_flutter/presentation/resources/assets_manager.dart';
+import 'package:advance_flutter/presentation/resources/language_manager.dart';
 import 'package:advance_flutter/presentation/resources/routes_manager.dart';
 import 'package:advance_flutter/presentation/resources/strings_manager.dart';
 import 'package:advance_flutter/presentation/resources/values_manager.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'dart:math' as math;
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -31,7 +34,11 @@ class _SettingsPageState extends State<SettingsPage> {
             style: Theme.of(context).textTheme.headline4,
           ).tr(),
           leading: SvgPicture.asset(ImageAssets.changeLangIc),
-          trailing: SvgPicture.asset(ImageAssets.settingsRightArrowIc),
+          trailing: Transform(
+            alignment: Alignment.center,
+            transform: Matrix4.rotationY(isRtl() ? math.pi : 0),
+            child: SvgPicture.asset(ImageAssets.settingsRightArrowIc),
+          ),
           onTap: () {
             _changeLanguage();
           },
@@ -42,7 +49,11 @@ class _SettingsPageState extends State<SettingsPage> {
             style: Theme.of(context).textTheme.headline4,
           ).tr(),
           leading: SvgPicture.asset(ImageAssets.contactUsIc),
-          trailing: SvgPicture.asset(ImageAssets.settingsRightArrowIc),
+          trailing: Transform(
+            alignment: Alignment.center,
+            transform: Matrix4.rotationY(isRtl() ? math.pi : 0),
+            child: SvgPicture.asset(ImageAssets.settingsRightArrowIc),
+          ),
           onTap: () {
             _contactUs();
           },
@@ -53,7 +64,11 @@ class _SettingsPageState extends State<SettingsPage> {
             style: Theme.of(context).textTheme.headline4,
           ).tr(),
           leading: SvgPicture.asset(ImageAssets.inviteFriendsIc),
-          trailing: SvgPicture.asset(ImageAssets.settingsRightArrowIc),
+          trailing: Transform(
+            alignment: Alignment.center,
+            transform: Matrix4.rotationY(isRtl() ? math.pi : 0),
+            child: SvgPicture.asset(ImageAssets.settingsRightArrowIc),
+          ),
           onTap: () {
             _inviteFriends();
           },
@@ -64,7 +79,11 @@ class _SettingsPageState extends State<SettingsPage> {
             style: Theme.of(context).textTheme.headline4,
           ).tr(),
           leading: SvgPicture.asset(ImageAssets.logoutIc),
-          trailing: SvgPicture.asset(ImageAssets.settingsRightArrowIc),
+          trailing: Transform(
+            alignment: Alignment.center,
+            transform: Matrix4.rotationY(isRtl() ? math.pi : 0),
+            child: SvgPicture.asset(ImageAssets.settingsRightArrowIc),
+          ),
           onTap: () {
             _logout();
           },
@@ -73,8 +92,13 @@ class _SettingsPageState extends State<SettingsPage> {
     );
   }
 
+  bool isRtl() {
+    return context.locale == ARABIC_LOCALE; // app is in arabic
+  }
+
   void _changeLanguage() {
-    // TODO: I will apply localisation later
+    _appPreferences.setLanguageChanged();
+    Phoenix.rebirth(context); // restart to apply language changes
   }
 
   void _contactUs() {
